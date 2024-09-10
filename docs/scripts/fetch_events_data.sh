acc1f4e867aa104cb6830bdbea954485b74abfb9
David Carpenter <david@cooperativeit.co.uk>
2024-09-10 16:34:34 +0100

    Updates the whole events thing for scraping BAC

--- docs/scripts/fetch_events_data.sh
+++ docs/scripts/fetch_events_data.sh
@@ -2,12 +2,19 @@
 
 #Events
 wget --no-check-certificate --output-document=../_data/events.csv "https://docs.google.com/spreadsheets/d/1-Eugy7Wfl0O2dSach2D2dOoE8JEW2tI3sqChuCvLUYg/export?format=csv&gid=0"
+#Events
+wget --no-check-certificate --output-document=../_data/BACevents.csv "https://docs.google.com/spreadsheets/d/1-Eugy7Wfl0O2dSach2D2dOoE8JEW2tI3sqChuCvLUYg/export?format=csv&gid=38705545"
 #Venues
 wget --no-check-certificate --output-document=../_data/venues.csv "https://docs.google.com/spreadsheets/d/1-Eugy7Wfl0O2dSach2D2dOoE8JEW2tI3sqChuCvLUYg/export?format=csv&gid=287605425"
 #Artists
 wget --no-check-certificate --output-document=../_data/artists.csv "https://docs.google.com/spreadsheets/d/1-Eugy7Wfl0O2dSach2D2dOoE8JEW2tI3sqChuCvLUYg/export?format=csv&gid=370499827"
 #Noticeboards
 wget --no-check-certificate --output-document=../_data/noticeboards.csv "https://docs.google.com/spreadsheets/d/1gFoIK0H3zuTZ1uGm4yngbIMqyPRhcwRW12xzx3-KZf4/export?format=csv&gid=506723206"
+
+# Remove the column headers of the BAC sheet
+#sed -i '1d' "../_data/BACevents.csv"
+# Append the BAC events to all events
+cat "../_data/BACevents.csv" >> "../_data/events.csv"
 
 echo $1
 echo $2
